bdf5939434ee73be77e602cb138672d89fc4ecb5
tarqs <gaming.tarquin@gmail.com>
2015-11-18 23:16:24 +0100

    Update MenuChapter.java

--- src/mathapp/MenuChapter.java
+++ src/mathapp/MenuChapter.java
@@ -1,5 +1,8 @@
 package mathapp;
 
+/**
+ * Created by Gebruiker on 14-11-2015.
+ */
 import javax.swing.*;
 
 public class MenuChapter extends JMenu {
@@ -9,10 +12,10 @@ public class MenuChapter extends JMenu {
     }
 
     private void makeChapters() {
-        String listChapters = "Hoofdstuk 1,Hoofdstuk 2,Hoofdstuk 3,Hoofdstuk 4,Hoofdstuk 5,Hoofdstuk 6,Hoofdstuk 7,Hoofdstuk 8,Hoofdstuk 9,Hoofdstuk 10,";
+        String listChapters = "Hoofdstuk 1,Hoofdstuk 2,Hoofdstuk 3,Hoofdstuk 4,Hoofdstuk 5,Hoofdstuk 6,Hoofdstuk 7,Hoofdstuk 8,Hoofdstuk 9,Hoofdstuk 10";
         String[] menuItemChapters = listChapters.split(",");
 
-        String listPars = "Paragraaf 1,Paragraaf 2,Paragraaf 3,Paragraaf 4,Paragraaf 5,";
+        String listPars = "Paragraaf 1,Paragraaf 2,Paragraaf 3,Paragraaf 4,Paragraaf 5";
         String[] menuItemPars = listPars.split(",");
 
         for(int i = 0; i < menuItemChapters.length; i += 1){
@@ -20,11 +23,12 @@ public class MenuChapter extends JMenu {
             for(int j = 0; j < menuItemPars.length; j += 1){
                 JMenuItem item = new JMenuItem(menuItemPars[j]);
                 menu.add(item);
-            }
-            if(i == 4){
-                menu.addSeparator();
+                //item.addActionListener(this);
             }
             this.add(menu);
+            if(i == 4){
+                this.addSeparator();
+            }
         }
     }
 }
